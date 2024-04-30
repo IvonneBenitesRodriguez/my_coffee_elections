951e9636a6f0b42543da273f70d64d2cb94ea13e
Ivonne <ivonnebenites2015@gmail.com>
2024-04-30 18:04:06 -0500

    Update MenuList.js file

--- src/helpers/MenuList.js
+++ src/helpers/MenuList.js
@@ -10,30 +10,36 @@ export const MenuList = [
     name: "Capuccino",
     image: Capuccino,
     price: 9.9,
+    description: "Coffee with beans from Egypt and milk",
   },
   {
     name: "Latte",
     image: Latte,
     price: 12,
+    description: "Oat mixed with nuts creamy milk latte ",
   },
   {
     name: "Expresso",
     image: Expresso,
     price: 7,
+    description: "100% Strong Tuscany italian coffee ",
   },
   {
     name: "Mocacchino",
     image: Mocacchino,
     price: 18,
+    description: "Dark Chocolate, and coconut milk",
   },
   {
     name: "Starbucks",
     image: Starbucks,
     price: 14,
+    description: "Traditional Chicago Starbucks coffee",
   },
   {
     name: "Chailatte",
     image: Chailatte,
     price: 10,
+    description: "Mumbai Flavours with cinnamon and milk",
   },
 ];
