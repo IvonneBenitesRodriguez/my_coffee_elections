d44c8f7f2549ae6b34279c7a4588c97fc09238bb
Ivonne <ivonnebenites2015@gmail.com>
2024-04-30 18:03:39 -0500

    Update component named MenuItem.jsx

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -1,12 +1,13 @@
 import { Menu } from "@mui/base";
 import React from "react";
 
-function MenuItem({ image, name, price }) {
+function MenuItem({ image, name, price, description }) {
   return (
     <div className="menuItem">
       <div style={{ backgroundImage: `url(${image})` }}></div>
       <h1> {name} </h1>
       <p> ${price} </p>
+      <span>{description}</span>
     </div>
   );
 }
