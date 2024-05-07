e5ee90a6aa60b9c861334a5614e33782ac9342bf
Ivonne <ivonnebenites2015@gmail.com>
2024-05-06 22:58:04 -0500

    Add new page named Recipes.jsx

--- src/pages/Recipes.jsx
+++ src/pages/Recipes.jsx
@@ -1,12 +1,51 @@
-import React from "react";
-import "../styles/Navbar.css";
+import React, { useState } from "react";
+import "../styles/Recipes.css";
 
 function Recipes() {
+  const [randomDogImage, setRandomDogImage] = useState(null);
+
+  const fetchRandomDogImage = async () => {
+    try {
+      const response = await fetch(`https://dog.ceo/api/breeds/image/random`);
+      if (!response.ok) {
+        throw new Error("Network response was not ok today");
+      }
+      const data = await response.json();
+      const breeds = Object.keys(data.message);
+      const randomIndex = Math.floor(Math.random() * breeds.length);
+      const randomBreed = breeds[randomIndex];
+      const randomRecipe = {
+        strCategory: randomBreed,
+        strCategoryDescription: `A delightful recipe involving the ${randomBreed} breed.`,
+        strCategoryImage: randomBreed,
+      };
+
+      setRandomRecipe(randomRecipe);
+    } catch (error) {
+      console.error("Error fetching recipes:", error);
+    }
+  };
+
   return (
-    <div>
-      <h2>
-        <a href="Recipes">Recipes</a>
-      </h2>
+    <div className="recipes">
+      <h1 className="recipeTitle">Enjoy our recipes</h1>
+      <p>
+        {""}
+        For a small meeting with your friends, or a celebration, you have a tons
+        of tasty recipes you can use, only assure yourself to click in this
+        button :
+      </p>
+      <button onClick={fetchRandomDogRecipe}>Open me</button>
+      {randomRecipe && (
+        <div className="randomRecipe">
+          <h2>{randomRecipe.strCategory}</h2>
+          <p>{randomRecipe.strCategoryDescription}</p>
+          <img
+            src={randomRecipe.strCategoryImage}
+            alt={`A ${randomRecipe.strCategory} dog`}
+          />
+        </div>
+      )}
     </div>
   );
 }
