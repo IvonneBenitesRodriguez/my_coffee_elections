9d1ec5ab5c774893132218cf7eb4df52515e7bc0
Ivonne <ivonnebenites2015@gmail.com>
2024-05-09 21:56:27 -0500

    Update Products.jsx

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,11 +1,12 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import "../styles/Products.css";
 
 function Products() {
   const [posts, setPosts] = useState([]);
 
   const fetchProducts = async () => {
-    const res = await axios.get("https://jsonplaceholder.typicode.com/photos");
+    const res = await axios.get("https://api.sampleapis.com/coffee/hot");
     console.log(res.data);
     setPosts(res.data);
     return res;
@@ -17,14 +18,22 @@ function Products() {
 
   return (
     <>
-      <div>Products</div>
+      <div className="menu">
+        <h1 className="menuTitle">Products</h1>
+      </div>
       {/* <button onClick={fetchProducts}>Fetch Products</button> */}
       <ul>
         {posts.slice(0, 10).map((post) => (
           <li key={post.id}>
-            <h3>{post.title}</h3>
-            <p>{post.body}</p>
-            <img src={post.url} alt={post.title} width={"120px"} />
+            <h3 className="apiTitle">{post.title}</h3>
+            <p className="apiParagraph">{post.description}</p>
+            <img
+              src={post.image}
+              className="apiImage"
+              alt={post.title}
+              width={"140px"}
+            />
+            <span className="apiSpan">{post.ingredients}</span>
           </li>
         ))}
       </ul>
