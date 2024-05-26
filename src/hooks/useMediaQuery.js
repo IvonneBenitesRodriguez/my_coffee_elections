af9fffca1b4d9b5aa036398634d7ba1d405ab57e
Ivonne <ivonnebenites2015@gmail.com>
2024-05-26 13:06:35 -0500

    Add folder named hooks

---
+++ src/hooks/useMediaQuery.js
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react";
+
+function useMediaQuery(query) {
+  const [matches, setMatches] = useState(
+    () => window.matchMedia(query).matches
+  );
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia(query);
+
+    const handleChange = () => {
+      setMatches(mediaQuery.matches);
+    };
+
+    handleChange();
+    mediaQuery.addEventListener("change", handleChange);
+
+    return () => {
+      mediaQuery.removeEventListener("change", handleChange);
+    };
+  }, [query]);
+
+  return matches;
+}
+
+export default useMediaQuery;
